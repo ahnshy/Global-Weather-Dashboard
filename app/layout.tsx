514f4e5144d3474a958ef3c1445352073a04fc45
ahnshy <ahnshy@naver.com>
2024-09-22 18:29:43 +0900

    <feat> : Initialize Project

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./Providers/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "WeatherDashboard",
+  description: "A Weather Dashboard built with Next.js and TailwindCSS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function WeatherDashboardPage() {
+  return (
+    <div>
+      <h1 className={title()}>WeatherDashboard</h1>
+    </div>
+  );
+}
